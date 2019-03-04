94203a9f2a90934a7daa7e3579dbc57830156fa8
Nks <niko.afv@gmail.com>
2019-03-04 11:15:14 -0300

    update home view, delete cache views files

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,7 +8,7 @@
                     <div class="col-lg-12">
                         <div class="text-center m-t-lg">
                             <h1>
-                                Welcome in INSPINIA Laravel Starter Project
+                                Bienvenido a la plataforma de clubes!
                             </h1>
                             <small>
                                 It is an application skeleton for a typical web app. You can use it to quickly bootstrap your webapp projects.
